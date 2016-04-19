0bf14466d21f05927705ab119b50ef474caac9e8
Danny Nguyen <danny@80pct.com>
2016-04-19 00:48:57 -0400

    Add initial solution for word-count

---
+++ word-count/word_count.exs
@@ -0,0 +1,25 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t) :: map
+  def count(sentence) do
+    words = sentence
+      |> remove_punctuation
+      |> String.downcase
+      |> String.split
+      |> count_words
+  end
+
+  defp remove_punctuation(sentence) do
+    String.replace(sentence, ~r/[^\p{L}\p{N}-]/u, " ")
+  end
+
+  defp count_words(words), do: count_words(words, %{})
+  defp count_words([], acc), do: acc
+  defp count_words([first|rest], acc) do
+    count_words(rest, Map.update(acc, first, 1, &(&1 + 1)))
+  end
+end
