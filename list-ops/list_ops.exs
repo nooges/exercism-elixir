b2fb1b7d98dde5381d88bae63bfe0d1528c9b9a6
Danny Nguyen <danny@80pct.com>
2016-05-04 12:23:02 -0400

    Implemented map

--- list-ops/list_ops.exs
+++ list-ops/list_ops.exs
@@ -7,22 +7,18 @@ defmodule ListOps do
   # automatically imported) and so shouldn't be used either.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-    case l do
-      [] -> 0
-      _ -> 1 + count(tl(l))
-    end
-  end
+  def count(l), do: _count(l, 0)
+  defp _count([], acc), do: acc
+  defp _count([head | tail], acc), do: _count(tail, acc + 1)
 
   @spec reverse(list) :: list
   def reverse(l), do: _reverse(l, [])
   defp _reverse([], acc), do: acc
-  defp _reverse([first | rest], acc), do: _reverse(rest, [first | acc])
+  defp _reverse([head | tail], acc), do: _reverse(tail, [head | acc])
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-
-  end
+  def map([], f), do: []
+  def map([head | tail], f), do: [f.(head) | map(tail, f)]
 
   @spec filter(list, (any -> as_boolean(term))) :: list
   def filter(l, f) do
--- list-ops/list_ops_test.exs
+++ list-ops/list_ops_test.exs
@@ -42,17 +42,17 @@ defmodule ListOpsTest do
     assert L.reverse(Enum.to_list(1..1_000_000)) == Enum.to_list(1_000_000..1)
   end
 
-  @tag :pending
+  #@tag :pending
   test "map of empty list" do
     assert L.map([], &(&1+1)) == []
   end
 
-  @tag :pending
+  #@tag :pending
   test "map of normal list" do
     assert L.map([1,3,5,7], &(&1+1)) == [2,4,6,8]
   end
 
-  @tag :pending
+  #@tag :pending
   test "map of huge list" do
     assert L.map(Enum.to_list(1..1_000_000), &(&1+1)) ==
       Enum.to_list(2..1_000_001)
