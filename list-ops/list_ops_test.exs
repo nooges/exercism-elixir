ff0f9077eebfc3ce8163864385fbd57bd63e1594
Danny Nguyen <danny@80pct.com>
2016-05-04 12:30:48 -0400

    Implemented reduce

--- list-ops/list_ops_test.exs
+++ list-ops/list_ops_test.exs
@@ -74,23 +74,23 @@ defmodule ListOpsTest do
       Enum.map(1..500_000, &(&1*2-1))
   end
 
-  @tag :pending
+  #@tag :pending
   test "reduce of empty list" do
     assert L.reduce([], 0, &(&1+&2)) == 0
   end
 
-  @tag :pending
+  #@tag :pending
   test "reduce of normal list" do
     assert L.reduce([1,2,3,4], -3, &(&1+&2)) == 7
   end
 
-  @tag :pending
+  #@tag :pending
   test "reduce of huge list" do
     assert L.reduce(Enum.to_list(1..1_000_000), 0, &(&1+&2)) ==
       Enum.reduce(1..1_000_000, 0, &(&1+&2))
   end
 
-  @tag :pending
+  #@tag :pending
   test "reduce with non-commutative function" do
     assert L.reduce([1,2,3,4], 10, fn x, acc -> acc - x end) == 0
   end
